3917d94c10c4e262602737657d536113c3177aba
3260385683 <c3260385683@163.com>
2020-08-27 14:50:12 +0800

    test

--- mr-shop-parent/mingrui-shop-basics/mingrui-shop-basic-eureka-server/src/main/java/com/baidu/RunEurekaServerApplication.java
+++ mr-shop-parent/mingrui-shop-basics/mingrui-shop-basic-eureka-server/src/main/java/com/baidu/RunEurekaServerApplication.java
@@ -16,6 +16,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class RunEurekaServerApplication {
 
     public static void main(String[] args) {
+        System.out.println("测试git");
         SpringApplication.run(RunEurekaServerApplication.class);
     }
 
